a5688c1c431e84beca1ea763c77d2161c295c756
MONYtry <tsr@gmx.de>
2026-07-30 13:43:44 +0200

    Added Comments to Config

--- config.py
+++ config.py
@@ -1,11 +1,21 @@
+################
+#    CONFIG    #
+#    small     #
+################
+
+# Camera Settings
 CAM_WIDTH = 640
 CAM_HEIGHT = 480
 
+# Perfomance delay (fps booster)
 YOLO_DELAY = 0.3
 EMOTION_DELAY = 1.0
 
+# Tracking Confidence
 CONFIDENCE = 0.3
 
+
+# Colors
 BLUE = (255,0,0)
 GREEN = (0,255,0)
 RED = (0,0,255)
